5aa2cb471b403e086d510e25578ad3e3033f02e2
Contex <contexmoh@gmail.com>
2012-07-10 12:38:50 +0200

    First release of the API, undocumented.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 XenForoAPI
 ==========
 
-This class can be used to use XenForo's function in other projects. One class to rule them all.
+This class can be used to use XenForo's function in other PHP projects. One class to rule them all.
+
+Copyright (c) 2011-2012, Contex <http://www.contex.me>
+
+XenForoAPI is licensed under GNU LESSER GENERAL PUBLIC LICENSE Version 3.
---
+++ me/contex/php/xenforo/XenForoAPI/XenForoAPI.php
@@ -0,0 +1,163 @@
+<?php
+/*
+ * This file is part of XenForoAPI <http://www.contex.me/>.
+ *
+ * XenForoAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XenForoAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+$xf = new XenForoAPI("/home/contex/www/"); 
+echo $xf->getLatestUser()->getID() . "<br>";
+if (!$xf->getUser(1)->validateAuthentication("test")) {
+	echo "Failed login!<br>";
+} else {
+	echo "Passed login!<br>";
+}
+echo $xf->countTotalUsers() . "<br>";
+echo $xf->getLatestUser()->getID();
+
+class XenForoAPI {
+	private $xfDir, $startTime;
+	private $models;
+	
+	public function __construct($directory) {
+		$this->xfDir = $directory;
+		$this->startTime = microtime(true);
+		require($this->xfDir . '/library/XenForo/Autoloader.php');
+		XenForo_Autoloader::getInstance()->setupAutoloader($this->xfDir. '/library');
+		XenForo_Application::initialize($this->xfDir . '/library', $this->xfDir);
+		XenForo_Application::set('page_start_time', $this->startTime);
+		$this->models = new XenForoModels();
+		$this->models->setUserModel(XenForo_Model::create('XenForo_Model_User'));
+		$this->models->setUserFieldModel(XenForo_Model::create('XenForo_Model_UserField'));
+	}
+	
+	public function getModels() {
+		return $this->models;
+	}
+	
+	public function getUserModel() {
+		return $this->models->getUserModel();
+	}
+	
+	public function getUserFieldModel() {
+		return $this->models->getUserFieldModel();
+	}
+	
+	public function getLatestUser() {
+		return new XenForoUser($this->models, $this->models->getUserModel()->getLatestUser());
+	}
+	
+	public function getTotalUsersCount() {
+		return $this->models->getUserModel()->countTotalUsers();
+	}
+	
+	public function getUser($userInput) {
+		if (is_int($userInput)) {
+			return new XenForoUser($this->models, $this->models->getUserModel()->getUserById($userInput));
+		} else if($this->models->getUserModel()->couldBeEmail($userInput)) {
+			return new XenForoUser($this->models, $this->models->getUserModel()->getUserByEmail($userInput));
+		} else {
+			return new XenForoUser($this->models, $this->models->getUserModel()->getUserByName($userInput));
+		}
+	}
+}
+
+class XenForoModels {
+	private $userModel, $userFieldModel;
+	
+	public function setUserModel($userModel) {
+		$this->userModel = $userModel;
+	}
+	
+	public function getUserModel() {
+		return $this->userModel;
+	}
+	
+	public function setUserFieldModel($userFieldModel) {
+		$this->userFieldModel = $userFieldModel;
+	}
+	
+	public function getUserFieldModel() {
+		return $this->userFieldModel;
+	}
+}
+
+class XenForoUser {
+	private $models, $data;
+	
+	public function __construct($models, $data) {
+		$this->models = $models;
+		$this->data = $data;
+	}
+	
+	public function getData() {
+		return $this->data;
+	}
+	
+	public function getAuthenticationRecord() {
+		return $this->models->getUserModel()->getUserAuthenticationRecordByUserId($this->getID()); 
+	}
+	
+	public function validateAuthentication($password) {
+		return $this->models->getUserModel()->validateAuthentication($this->getUsername(), $password); 
+	}
+	
+	public function getUnreadAlertsCount() {
+		return $this->models->getUserModel()->getUnreadAlertsCount($this->getID()); 
+	}
+
+	public function getID() {
+		return $this->data['user_id'];
+	}
+	
+	public function getUsername() {
+		return $this->data['username'];
+	}
+	
+	public function getEmail() {
+		return $this->data['email'];
+	}
+	
+	public function getGender() {
+		return $this->data['gender'];
+	}
+	
+	public function getCustomTitle() {
+		return $this->data['custom_title'];
+	}
+	
+	public function getLanguageID() {
+		return $this->data['language_id'];
+	}
+	
+	public function getStyleID() {
+		return $this->data['style_id'];
+	}
+	
+	public function getTimezone() {
+		return $this->data['timezone'];
+	}
+	
+	public function isVisible() {
+		return $this->data['visible'];
+	}
+	
+	public function getGroupID() {
+		return $this->data['user_group_id'];
+	}
+	
+	public function getSecondaryGroupIDs() {
+		return $this->data['secondary_group_ids'];
+	}
+}
+?>
