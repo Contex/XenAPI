ff6239784e40b249ffdb31af875e06e0eb6816fa
Contex <contexmoh@gmail.com>
2014-06-02 20:21:53 +0200

    Updated version to 1.4

--- net/xenapi/XenAPI/api.php
+++ net/xenapi/XenAPI/api.php
@@ -56,7 +56,7 @@ if ($restAPI->getAPIKey() != NULL && $restAPI->getAPIKey() == 'REPLACE_THIS_WITH
 $restAPI->processRequest();
 
 class RestAPI {
-    const VERSION = '1.4.dev';
+    const VERSION = '1.4';
     const GENERAL_ERROR = 0x201;
     const USER_ERROR = 0x202;
     /**
